2cbf48b78faf8b9d498ae119e7555e704f82ab73
Marcel van der Veldt <m.vanderveldt@outlook.com>
2020-11-01 11:28:19 +0100

    small fixes

--- music_assistant/rootfs/etc/cont-init.d/mass.sh
+++ music_assistant/rootfs/etc/cont-init.d/mass.sh
@@ -10,3 +10,11 @@ if bashio::config.true 'use_nightly'; then
     bashio::log.green "Updating to latest (prerelease) version..."
     pip install --upgrade --pre music-assistant
 fi
+
+if bashio::config.true 'debug_messages'; then
+    bashio::log.green "Debug messages are enabled..."
+    printf "true" > /var/run/s6/container_environment/DEBUG
+else
+    bashio::log.green "Debug messages are disabled..."
+    printf "false" > /var/run/s6/container_environment/DEBUG
+fi
