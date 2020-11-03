b7e85eb38bda68e19650ebfc3c81436d37cc9fa9
Marcel van der Veldt <m.vanderveldt@outlook.com>
2020-11-03 12:39:04 +0100

    update music assistant

--- music_assistant/run.sh
+++ music_assistant/run.sh
@@ -1,20 +1,24 @@
-#!/usr/bin/with-contenv bashio
+#!/usr/bin/bash
 # ==============================================================================
 # Home Assistant Community Add-on: MusicAssistant
 # Configures MusicAssistant
 # ==============================================================================
 
 
-# Install latest version from PyPi
+# Install latest version from git
 if bashio::config.true 'use_nightly'; then
-    bashio::log.green "Updating to latest (prerelease) version..."
-    pip install --upgrade --pre music-assistant
+    bashio::log.green "Updating to latest nightly version..."
+    cd /tmp
+    curl -L https://github.com/music-assistant/server/archive/master.tar.gz | tar xz
+    cd server-master
+    python3 setup.py install
+    rm -rf /tmp/*
 fi
 
 if bashio::config.true 'debug_messages'; then
     bashio::log.green "Debug messages are enabled..."
-    printf "true" > /var/run/s6/container_environment/DEBUG
+    mass --config /data --debug
 else
     bashio::log.green "Debug messages are disabled..."
-    printf "false" > /var/run/s6/container_environment/DEBUG
+    mass --config /data
 fi
