60d966325d8a53685af61078baf887c110705814
Marcel van der Veldt <m.vanderveldt@outlook.com>
2021-01-21 22:05:02 +0100

    change order

--- zwave-js-server/run.sh
+++ zwave-js-server/run.sh
@@ -5,10 +5,10 @@ SERVER_SOURCE=$(cat /data/options.json | jq -r '.server_branch_or_commit')
 DRIVER_SOURCE=$(cat /data/options.json | jq -r '.driver_branch_or_commit')
 
 cd /usr/src/app
-echo "Fetching latest server version from $SERVER_SOURCE..."
-npm install ${SERVER_SOURCE}
 echo "Fetching latest driver version from $DRIVER_SOURCE..."
 npm install ${DRIVER_SOURCE}
+echo "Fetching latest server version from $SERVER_SOURCE..."
+npm install ${SERVER_SOURCE}
 
 echo "Start Z-Wave Js Server using $SERIAL_PORT"
 exec ./node_modules/.bin/zwave-server "$SERIAL_PORT"
