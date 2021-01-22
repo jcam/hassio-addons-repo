ef99ccaa0ca95178cf6fd5f34383a1818b7d404a
Marcel van der Veldt <m.vanderveldt@outlook.com>
2021-01-22 10:22:13 +0100

    bump zwavejs

--- zwave-js-server/run.sh
+++ zwave-js-server/run.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
 
 SERIAL_PORT=$(cat /data/options.json | jq -r '.serial_port')
-SERVER_SOURCE=$(cat /data/options.json | jq -r '.server_branch_or_commit')
-DRIVER_SOURCE=$(cat /data/options.json | jq -r '.driver_branch_or_commit')
+SERVER_SOURCE=$(cat /data/options.json | jq -r '.branch_or_commit')
 
 cd /usr/src/app
-echo "Fetching latest driver version from $DRIVER_SOURCE..."
-npm install ${DRIVER_SOURCE}
 echo "Fetching latest server version from $SERVER_SOURCE..."
 npm install ${SERVER_SOURCE}
 
 echo "Start Z-Wave Js Server using $SERIAL_PORT"
-exec ./node_modules/.bin/zwave-server "$SERIAL_PORT"
+exec ./node_modules/.bin/zwave-server "$SERIAL_PORT" --config /data/options.json
 
