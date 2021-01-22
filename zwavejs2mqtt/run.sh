ce0f2dc738d41aaf41cd91a6fd4b11a2eab4a291
Marcel van der Veldt <m.vanderveldt@outlook.com>
2021-01-23 00:27:00 +0100

    zw2js

--- zwavejs2mqtt/run.sh
+++ zwavejs2mqtt/run.sh
@@ -8,9 +8,24 @@ export NETWORK_KEY=$NETWORK_KEY
 export STORE_DIR=$DATA_DIR
 export SERIAL_PORT=$SERIAL_PORT
 
+echo "NETWORK_KEY=$NETWORK_KEY"
+echo "STORE_DIR=$DATA_DIR"
+echo "SERIAL_PORT=$SERIAL_PORT"
+
+# update to latest version from master if enabled
 if [ "$USE_DEV" = "true" ]; then
     echo "Update to latest dev version of Zwave JS 2 MQTT..."
     npm install zwave-js/zwavejs2mqtt#master
 fi
 
-exec node bin/www
+# default config
+FILE=/data/settings.json
+if [ ! -f "/data/settings.json" ]; then
+    mv /data/default_settings.json /data/settings.json
+fi
+# update config
+jq -c '.zwave.port = "$SERIAL_PORT"' /data/settings.json > tmp.$$.json && mv tmp.$$.json /data/settings.json
+jq -c '.zwave.networkKey = "$NETWORK_KEY"' /data/settings.json > tmp.$$.json && mv tmp.$$.json /data/settings.json
+
+nginx -g 'pid /tmp/nginx.pid;'
+nginx && node bin/www
